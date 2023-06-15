1701cf5cbcd90df212542a8ecf0e63c11349aec0
Sasa Savic Perusina <sasa@bubulazy.com>
2023-06-15 10:24:03 +0100

    Invoking Sserveless function

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
 import "./App.css";
 
 function App() {
+  const url =
+    "https://listillo-openai-api.netlify.app/.netlify/functions/openAiFetch";
+
+  async function fetchApi() {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "text/plain",
+      },
+      body: "Jujuuuu",
+    });
+    const data = await response.json();
+    console.log(data);
+  }
+
   return <h1>Test</h1>;
 }
 
